12b47e69b157d24cb9a289ffb5145ed11a1ffc9c
Roger Silva <rogersilva84@gmail.com>
2017-06-11 12:19:45 -0300

    Initial commit

---
+++ WpfApp11/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Texto_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (sender as ComboBox);
+            ComboBoxItem item = (combo.SelectedItem as ComboBoxItem);
+            SolidColorBrush cor = (SolidColorBrush)new BrushConverter().ConvertFromString(item.DataContext as string);
+
+            if (this.Area != null)
+            {
+                this.Area.Foreground = cor;
+            }
+        }
+
+        private void Fundo_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (sender as ComboBox);
+            ComboBoxItem item = (combo.SelectedItem as ComboBoxItem);
+            SolidColorBrush cor = (SolidColorBrush)new BrushConverter().ConvertFromString(item.DataContext as string);
+
+            if (this.Area != null)
+            {
+                this.Area.Background = cor;
+            }
+        }
+    }
+}
